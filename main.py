0ea250fc91162ef90e6dd52535f17f0d3ebe5444
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2022-10-10 09:43:14 +0300

    Start

---
+++ main.py
@@ -0,0 +1,28 @@
+def create_table():
+	"""Create table in database"""
+
+def add_client():
+	"""Add new client in table"""
+
+def add_phone():
+	"""Add phone number for existing client"""
+
+def change_client_data():
+	"""Change client info in database"""
+
+def del_phone():
+	"""Delete phone number from existing client"""
+
+def del_client():
+	"""Delete client from table"""
+
+def finde_client():
+	"""Finde client in database"""
+
+def main():
+	"""Main function"""
+
+	print('Text')
+
+if __name__ == '__main__':
+	main()
